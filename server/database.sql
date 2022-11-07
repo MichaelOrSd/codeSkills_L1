a50f7dc02ca0f192af294792f1a6bbbdb98e1998
Michael Oreilly <michael.oreilly@keyin.com>
2022-11-07 10:06:22 -0330

    package json updates

---
+++ server/database.sql
@@ -0,0 +1,9 @@
+CREATE DATEBASE pern;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+);
