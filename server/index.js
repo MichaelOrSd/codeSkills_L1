347172dc98bee0637b77f39b02409b687a59dc97
Michael Oreilly <michael.oreilly@keyin.com>
2022-11-08 12:20:49 -0330

    create and getAll users

--- server/index.js
+++ server/index.js
@@ -9,23 +9,36 @@ app.use(express.json());
 
 // Routes
 
+// Create a user
 app.post("/users", async(req, res) => {
     try {
-        console.log(req.body);
+        const { first_name, last_name, email, created_at } = req.body;
+        const newUser = await pool.query("INSERT INTO users (first_name, last_name, email, created_at) VALUES ($1, $2, $3, $4) RETURNING *", 
+        [first_name, last_name, email, created_at]
+        );
+        res.json(newUser.rows[0]);
+
     } catch (err) {
         console.error(err.message);
     }
 })
 
-// Create a todo
+// Get all users
 
-// Get all todos
+app.get("/users", async(req, res) => {
+    try {
+        const allUsers = await pool.query("SELECT * FROM users");
+        res.json(allUsers.rows);
+    } catch (error) {
+        console.error(err.message);
+    }
+})
 
-// Get a todo
+// Get a user
 
-// Update a todo
+// Update a user
 
-// Delete a todo
+// Delete a user
 
 
 app.listen(5000, () => {
