bf5eec5289cc028044f2f4e34989464e49bdb8db
Michael Oreilly <michael.oreilly@keyin.com>
2022-11-08 11:48:04 -0330

    updates to index.js

--- server/index.js
+++ server/index.js
@@ -1,9 +1,32 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
+// Middleware
 app.use(cors());
 app.use(express.json());
+
+// Routes
+
+app.post("/users", async(req, res) => {
+    try {
+        console.log(req.body);
+    } catch (err) {
+        console.error(err.message);
+    }
+})
+
+// Create a todo
+
+// Get all todos
+
+// Get a todo
+
+// Update a todo
+
+// Delete a todo
+
 
 app.listen(5000, () => {
     console.log("Server is running on port 5000");
