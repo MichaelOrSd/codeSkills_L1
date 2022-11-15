4eb87bd8e67e8cc1d6e75651d9a5c4105e85a20d
Michael Oreilly <michael.oreilly@keyin.com>
2022-11-15 16:09:08 -0330

    revert to previous

--- server/db.js
+++ server/db.js
@@ -1,14 +1,11 @@
 const Pool = require("pg").Pool;
-const dotenv = require("dotenv");
-
-dotenv.config();
 
 const pool = new Pool({
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME
+    user: "postgres",
+    password: "postgres",
+    host: "localhost",
+    port: 5432,
+    database: "pern"
 });
 
 module.exports = pool;
